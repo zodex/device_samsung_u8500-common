593def962989fa048df9d7974e41d701c458ef15
Robert Rozic <rooobertinhooo@gmail.com>
2013-11-22 16:58:42 +0100

    Fix LPM typo

--- common.mk
+++ common.mk
@@ -19,7 +19,7 @@ DEVICE_PACKAGE_OVERLAYS := $(COMMON_PATH)/overlay
 
 # Init files
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/rootdir/lpm.rc:root/lpm.rc
+    $(COMMON_PATH)/rootdir/lpm.rc:root/lpm.rc
 
 # STE
 PRODUCT_COPY_FILES += \
