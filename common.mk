5e5928aca6d31a08b7e6ce58cadea2dd6c2a495e
Franco Rapetti <frapeti@gmail.com>
2013-12-07 20:10:18 -0300

    Revert "Use SamsungExynos4RIL class for test"
    This reverts commit b820bb2faf31ac18e3727b67a300fe590649d554.

--- common.mk
+++ common.mk
@@ -142,7 +142,7 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     mobiledata.interfaces=pdp0,wlan0,gprs,ppp0 \
     ro.telephony.default_network=0 \
-    ro.telephony.ril_class=SamsungExynos4RIL \
+    ro.telephony.ril_class=SamsungU8500RIL \
     ro.telephony.sends_barcount=1
 		
 PRODUCT_PROPERTY_OVERRIDES += \
